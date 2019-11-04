3d0b37e7098300cea89c81868cbf519dee50c1f6
duyi <1079240024@qq.com>
2019-11-04 19:29:25 +0800

    设置url

--- app/src/main/java/com/duyi/webviewdemo/WebActivity.kt
+++ app/src/main/java/com/duyi/webviewdemo/WebActivity.kt
@@ -19,8 +19,8 @@ class WebActivity : AppCompatActivity() {
         webSettings.javaScriptEnabled = true
 
 //        web.loadUrl("https://www.baidu.com/")
-        web.loadUrl("https://www.independent.co.uk/news/world/europe/glacier-funeral-switzerland-pivol-climate-change-global-warming-a9116046.html")
-
+//        web.loadUrl("https://www.independent.co.uk/news/world/europe/glacier-funeral-switzerland-pivol-climate-change-global-warming-a9116046.html")
+        web.loadUrl("https://post.smzdm.com/p/awxqz594/")
         //系统默认会通过手机浏览器打开网页，为了能够直接通过WebView显示网页，则必须设置
         web.webViewClient = object:WebViewClient(){
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
