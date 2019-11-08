2cf5785abe8561b5f4a0140a4089e085746490f7
exueyuan <1079240024@qq.com>
2019-11-08 10:59:59 +0800

    修改链接

--- app/src/main/java/com/duyi/webviewdemo/WebActivity.kt
+++ app/src/main/java/com/duyi/webviewdemo/WebActivity.kt
@@ -20,7 +20,9 @@ class WebActivity : AppCompatActivity() {
 
 //        web.loadUrl("https://www.baidu.com/")
 //        web.loadUrl("https://www.independent.co.uk/news/world/europe/glacier-funeral-switzerland-pivol-climate-change-global-warming-a9116046.html")
-        web.loadUrl("https://post.smzdm.com/p/awxqz594/")
+        web.loadUrl("https://heycar.m.autohome.com.cn/Flipboard/m/74976943-1.html")
+
+//        web.loadUrl("https://post.smzdm.com/p/awxqz594/")
         //系统默认会通过手机浏览器打开网页，为了能够直接通过WebView显示网页，则必须设置
         web.webViewClient = object:WebViewClient(){
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
