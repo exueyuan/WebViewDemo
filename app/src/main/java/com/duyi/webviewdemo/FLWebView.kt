de0b5c62b6ad2baf92bebe9a0f2ae89e7b12b030
exueyuan <1079240024@qq.com>
2019-11-08 12:40:51 +0800

    处理webview慢慢增大的问题

---
+++ app/src/main/java/com/duyi/webviewdemo/FLWebView.kt
@@ -0,0 +1,22 @@
+package com.duyi.webviewdemo
+
+import android.content.Context
+import android.util.AttributeSet
+import android.webkit.WebView
+
+class FLWebView :WebView{
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    var sizeChangeCallBack:((w: Int, h: Int, ow: Int, oh: Int)->Unit)? = null
+
+    override fun onSizeChanged(w: Int, h: Int, ow: Int, oh: Int) {
+        super.onSizeChanged(w, h, ow, oh)
+        sizeChangeCallBack?.invoke(w, h, ow, oh)
+    }
+}
--- app/src/main/java/com/duyi/webviewdemo/WebActivity.kt
+++ app/src/main/java/com/duyi/webviewdemo/WebActivity.kt
@@ -1,8 +1,12 @@
 package com.duyi.webviewdemo
 
 import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import android.webkit.*
 import kotlinx.android.synthetic.main.activity_web.*
 
@@ -14,13 +18,25 @@ class WebActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
 
+        fl_root.viewTreeObserver.addOnGlobalLayoutListener(object :ViewTreeObserver.OnGlobalLayoutListener{
+            override fun onGlobalLayout() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    fl_root.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                }
+                val param = web.layoutParams
+                param.height = fl_root.height
+                web.layoutParams = param
+            }
+        })
+
+
         //设置javascript
         val webSettings = web.getSettings()
         webSettings.javaScriptEnabled = true
 
-//        web.loadUrl("https://www.baidu.com/")
+        web.loadUrl("https://www.baidu.com/")
 //        web.loadUrl("https://www.independent.co.uk/news/world/europe/glacier-funeral-switzerland-pivol-climate-change-global-warming-a9116046.html")
-        web.loadUrl("https://heycar.m.autohome.com.cn/Flipboard/m/74976943-1.html")
+//        web.loadUrl("https://heycar.m.autohome.com.cn/Flipboard/m/74976943-1.html")
 
 //        web.loadUrl("https://post.smzdm.com/p/awxqz594/")
         //系统默认会通过手机浏览器打开网页，为了能够直接通过WebView显示网页，则必须设置
@@ -28,6 +44,13 @@ class WebActivity : AppCompatActivity() {
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
                 view?.loadUrl(url)
                 return true
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                val param = web.layoutParams
+                param.height = ViewGroup.LayoutParams.MATCH_PARENT
+                web.layoutParams = param
             }
         }
 
@@ -40,6 +63,11 @@ class WebActivity : AppCompatActivity() {
                     tv_progress.text = "进度：100%"
                 }
             }
+        }
+
+        web.sizeChangeCallBack = { w: Int, h: Int, ow: Int, oh: Int ->
+
+            Log.i("hahahhah","w:$w,h:$h")
         }
     }
 }
