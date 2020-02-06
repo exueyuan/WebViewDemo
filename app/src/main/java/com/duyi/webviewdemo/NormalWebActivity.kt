a726412de7e8da7e88ae9bca2cfdb81d7ca75605
duyi <1079240024@qq.com>
2020-02-06 19:51:01 +0800

    修改url

--- app/src/main/java/com/duyi/webviewdemo/NormalWebActivity.kt
+++ app/src/main/java/com/duyi/webviewdemo/NormalWebActivity.kt
@@ -42,6 +42,7 @@ class NormalWebActivity : AppCompatActivity() {
 
         web.loadUrl("https://mp.weixin.qq.com/s?__biz=MzI5MTczODI3Ng%3D%3D&chksm=ec0e54e2db79ddf4e4a30d639c7f46229016a0a763ad6de67f4b04e12596be7840cc4571e4bd&idx=1&mid=2247553833&scene=0&sn=41154e31aabe6c3390fccdac83ee60d3&xtrack=1#rd")
 
+//        web.loadUrl("https://www.flipboard.cn/hybrid/articles/http%3A%2F%2Fm.happyjuzi.com%2Farticle%2F193166.html%3Fd_source%3Dflipboard%26pf%3Dandroid%26view%3D32?section_id=flipboard%2Fcurator%252Fmagazine%252FU6z-oRJ_QYSqF_vtoEstrw%253Am%253A995395684&section_subscribed=false")
         //系统默认会通过手机浏览器打开网页，为了能够直接通过WebView显示网页，则必须设置
         web.webViewClient = object:WebViewClient(){
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
